06b9646697024ec0bbe6411c7839bbec67732aff
dimitrilzk <dimitrilazuka95@gmail.com>
2023-05-25 16:08:20 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //public IActionResult Details([FromRoute(Name = "id")] string CourseId, [FromQuery(Name = "p")] string prova)
+        //{
+        //    return Content($"Sono Detail con id {CourseId}");
+        //}
+        public IActionResult Index()
+        {
+            return Content("Sono Index");
+        }
+        public IActionResult Details(string id)
+        {
+            return Content($"Sono Detail con id {id}");
+        }
+
+    }
+}
