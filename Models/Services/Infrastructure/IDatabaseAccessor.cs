2eaf8edfbe9c0e7aaa9cb3b3e704237f4ebb2b8b
dimitrilzk <dimitrilazuka95@gmail.com>
2023-06-06 18:30:35 +0200

    completato sezione 10

---
+++ Models/Services/Infrastructure/IDatabaseAccessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace WebApplication4.Models.Services.Infrastructure
+{
+    public interface IDatabaseAccessor
+    {
+        DataSet Query(string query);
+    }
+}
---
+++ Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace WebApplication4.Models.Services.Infrastructure
+{
+    public class SqliteDatabaseAccessor : IDatabaseAccessor
+    {
+        public DataSet Query(string query)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
